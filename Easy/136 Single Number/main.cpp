c85c5f89825d77203428b96885ce13e2f8167936
Lei Gao <gaolei.ioz@gmail.com>
2016-10-11 13:16:48 -0500

    [Single Number][CPP XOR]

--- Easy/136 Single Number/main.cpp
+++ Easy/136 Single Number/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Single_Number{
+public:
+  //constructor;
+  Single_Number(vector<int>& v) : nums(v) {}
+
+  // function of finding single number, which require a linear runtime.
+  int singlenumber(){
+    int res = 0;
+    for(int i = 0; i < nums.size(); i++){
+      res ^= nums[i] ;
+    }
+    return res;
+  }
+
+public:
+  vector<int> nums;
+};
+
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  vector<int> a = {1,2,3,4,5,6,1,2,3,4,5};
+  Single_Number test(a);
+  test.singlenumber();
+  cout << "the single number is " << test.singlenumber() << endl;
   return 0;
 }
