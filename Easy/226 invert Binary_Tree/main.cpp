85ba63ddde5d43c0458f1a382c8b23a2308f00f3
Lei Gao <gaolei.ioz@gmail.com>
2016-11-09 10:41:04 -0600

    [easy linked list]

--- Easy/226 invert Binary_Tree/main.cpp
+++ Easy/226 invert Binary_Tree/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+/**
+ * Definition for a binary tree node.*/
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/** invert binary tree */
+class Solution {
+public:
+  TreeNode* invertTree(TreeNode* root) {
+    if(!root){
+      return NULL;
+    }
+
+    TreeNode* temp = root->left;
+    root->left = invertTree(root->right);
+    root->right = invertTree(temp);
+    return root;
+  }
+};
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
